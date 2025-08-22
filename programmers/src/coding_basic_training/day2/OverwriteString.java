2b91d19114459fb39a8c5f8e1126832a3ef953e2
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-08-22 23:33:00 +0900

    programmers basic training day2 last problem - 문자열겹쳐쓰기

--- programmers/src/coding_basic_training/day2/OverwriteString.java
+++ programmers/src/coding_basic_training/day2/OverwriteString.java
@@ -1,8 +1,32 @@
 package coding_basic_training.day2;
 
+import java.util.Arrays;
+
 public class OverwriteString {
 
     public static void main(String[] args) {
 
+        String answer = solution("He11oWor1d", "lloWorl", 2);
+
+        System.out.println(answer);
+
+    }
+
+    public static String solution(String my_string, String overwrite_string, int s) {
+
+        String answer = "";
+
+        char[] chars = my_string.toCharArray();
+        char[] overchars = overwrite_string.toCharArray();
+
+        for (int i = s; (i - s)  < overwrite_string.length() ; i++) {
+
+            chars[i] = overchars[i - s];
+
+        }
+
+        answer = String.valueOf(chars);
+
+        return answer;
     }
 }
