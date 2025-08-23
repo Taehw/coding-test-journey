f1245693a452d9d7bd57b08901dd8349471689fc
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-08-23 23:59:16 +0900

    programmers basic training day3 2. 문자열리스트를 문자열로 변환

--- programmers/src/coding_basic_training/day3/CharListToStringConverter.java
+++ programmers/src/coding_basic_training/day3/CharListToStringConverter.java
@@ -1,4 +1,24 @@
 package coding_basic_training.day3;
 
 public class CharListToStringConverter {
+
+    public static void main(String[] args) {
+
+        String[] arr = {"a", "b", "c"};
+
+        String result = Solution(arr);
+
+        System.out.println("result = " + result);
+    }
+
+    public static String Solution(String[] arr) {
+
+        String answer = "";
+
+        for (String string : arr) {
+            answer = answer.concat(string);
+        }
+
+        return answer;
+    }
 }
