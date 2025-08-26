546a152d430dd2639fd56b02c67b95bfc7223eb7
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-08-26 22:23:49 +0900

    programmers basic training day3 3. 문자열 곱하기

---
+++ baekjoon/bronze/array_list/boj_1546_doit.java
@@ -0,0 +1,8 @@
+package bronze.array_list;
+
+public class boj_1546_doit {
+
+    public static void main(String[] args) {
+
+    }
+}
--- programmers/src/coding_basic_training/day3/StringMultiplier.java
+++ programmers/src/coding_basic_training/day3/StringMultiplier.java
@@ -1,4 +1,23 @@
 package coding_basic_training.day3;
 
 public class StringMultiplier {
+
+    public static void main(String[] args) {
+
+        String result = solution("love", 10);
+
+        System.out.println("result = " + result);
+    }
+
+    public static String solution(String my_string, int k) {
+        String answer = "";
+
+        answer = my_string;
+
+        for (int i = 1; i < k; i++) {
+            answer = answer.concat(my_string);
+        }
+
+        return answer;
+    }
 }
