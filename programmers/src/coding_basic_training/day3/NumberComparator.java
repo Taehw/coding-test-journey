c666adb89f65b55300f20374d040b31ca531c10f
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-08-27 23:55:23 +0900

    programmers basic training day3 4. 두 수의 연산값 비교하기 풀긴했는데 자동완성에 의존함.

--- programmers/src/coding_basic_training/day3/NumberComparator.java
+++ programmers/src/coding_basic_training/day3/NumberComparator.java
@@ -1,4 +1,30 @@
 package coding_basic_training.day3;
 
 public class NumberComparator {
+
+    public static void main(String[] args) {
+
+        int result = solution(2, 91);
+        System.out.println("result = " + result);
+
+    }
+
+    public static int solution(int a, int b) {
+
+        int answer = 0;
+
+        String sa = String.valueOf(a);
+        String sb = String.valueOf(b);
+
+        int mul = 2 * a * b;
+        int sab = Integer.parseInt(sa + sb);
+
+        if (mul > sab) {
+            answer = mul;
+        } else {
+            answer = sab;
+        }
+
+        return answer;
+    }
 }
