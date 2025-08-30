d90a6d4a9c88406d3199e2b7a5a818467261cf05
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-08-30 23:49:06 +0900

    feat : BOJ 11659 구간 합 구하기4 - 에러수정및 정답 답안 제출 완료, 추후 다른 풀이 보면서 공부 필요

--- baekjoon/silver/boj_11659.java
+++ baekjoon/silver/boj_11659.java
@@ -11,24 +11,29 @@ public class boj_11659 {
         int N = sc.nextInt();
         int M = sc.nextInt();
 
-        int[] numbers = new int[N];
-        for (int i = 0; i < N; i++) {
+        int[] numbers = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
             numbers[i] = sc.nextInt();
         }
 
+//        for (int number : numbers) {
+//            System.out.println("number = " + number);
+//        }
+        
         //합 배열 만들기
-        int[] S = new int[N];
+        int[] S = new int[N+1];
 
         S[0] = numbers[0];
 
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             S[i] = S[i - 1] + numbers[i];
         }
 
 
-        for (int s : S) {
-            System.out.println("s = " + s);
-        }
+//        for (int s : S) {
+//            System.out.println("s = " + s);
+//        }
 
         int[] result = new int[M];
 
@@ -36,7 +41,7 @@ public class boj_11659 {
             int start = sc.nextInt();
             int end = sc.nextInt();
 
-            result[i] = S[end] - numbers[start];
+            result[i] = S[end] - S[start-1];
         }
 
         for (int R : result) {
