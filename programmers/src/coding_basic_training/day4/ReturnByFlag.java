341c686b6ff648893bf5bfc0b9b8368283f4ca70
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-09-01 17:15:29 +0900

    programmers basic training day4 5. flag에 따른 다른 값 반환하기

--- programmers/src/coding_basic_training/day4/ReturnByFlag.java
+++ programmers/src/coding_basic_training/day4/ReturnByFlag.java
@@ -3,6 +3,23 @@ package coding_basic_training.day4;
 public class ReturnByFlag {
 
     public static void main(String[] args) {
-        
+
+        int result1 = solution(-4, 7, true);
+        int result2 = solution(-4, 7, false);
+
+        System.out.println("result1 = " + result1);
+        System.out.println("result2 = " + result2);
+    }
+
+    public static int solution(int a, int b, boolean flag) {
+        int answer = 0 ;
+
+        if (flag) {
+            answer = a + b;
+        } else {
+            answer = a - b;
+        }
+
+        return answer;
     }
 }
