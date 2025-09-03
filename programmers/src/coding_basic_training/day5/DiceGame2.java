07616a7e4c122af96ffa1ccacae4901649e26747
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-09-03 15:13:06 +0900

    programmers basic training day5 2. 주사위 게임2 - if문사용으로 해결 - 추천 답안들 가독성이 좋은지 모르겠음

--- programmers/src/coding_basic_training/day5/DiceGame2.java
+++ programmers/src/coding_basic_training/day5/DiceGame2.java
@@ -1,4 +1,23 @@
 package coding_basic_training.day5;
 
 public class DiceGame2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution (int a, int b, int c){
+
+        int answer = 0;
+
+        if (a != b && a != c && b != c) {
+            answer = a+b+c;
+        } else if (a == b && b == c) {
+            answer = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+        } else {
+            answer = (a + b + c) * (a * a + b * b + c * c);
+        }
+
+        return answer;
+    }
 }
