30f11eacae2e62b6c55f60abe4fa42795dc1bc3e
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-09-04 22:29:59 +0900

    programmers basic training day5 3. 원소들의 곱과합 - 제곱에 대한 접근 math 라이브러리 이용

--- programmers/src/coding_basic_training/day5/ProductAndSumOfElements.java
+++ programmers/src/coding_basic_training/day5/ProductAndSumOfElements.java
@@ -2,5 +2,32 @@ package coding_basic_training.day5;
 
 public class ProductAndSumOfElements {
 
+    public static void main(String[] args) {
+
+
+        int[] num_list = {5,7,8,3};
+
+        int result = solution(num_list);
+        System.out.println("result = " + result);
+    }
+
+    public static int solution(int[] num_list) {
+
+        int answer = 0;
+
+        int product = 1;
+        int sum = 0;
+
+        for (int i : num_list) {
+            product *= i;
+            sum += i;
+        }
+
+        if (product < (sum * sum)) {
+            answer = 1;
+        }
+
+        return answer;
+    }
 
 }
