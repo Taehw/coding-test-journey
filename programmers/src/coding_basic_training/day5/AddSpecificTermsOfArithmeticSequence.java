c549d02b17133a604e7f57e1c488e45d322c2fe8
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-09-10 15:57:30 +0900

    programmers basic training day5 4. 등차수열의 특정한 항만 더하기 - 매우 easy

--- programmers/src/coding_basic_training/day5/AddSpecificTermsOfArithmeticSequence.java
+++ programmers/src/coding_basic_training/day5/AddSpecificTermsOfArithmeticSequence.java
@@ -1,4 +1,23 @@
 package coding_basic_training.day5;
 
+//등차수열의 특정한 항만 더하기
 public class AddSpecificTermsOfArithmeticSequence {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int a, int d, boolean[] included) {
+
+        int answer = 0;
+
+        //등차수열 공식 -> a+dn, n을 그냥 i라고 생각하면 됨
+        for (int i = 0; i < included.length; i++) {
+            if (included[i]) {
+                answer += a + i*d;
+            }
+        }
+
+        return answer;
+    }
 }
