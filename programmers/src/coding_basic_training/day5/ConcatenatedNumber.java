73e8e41059ea915f6f1b38a635297043b40dc87c
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-09-14 11:39:08 +0900

    programmers basic training day5 last problem - 코드처리하기 - 추천답안괜찮아보임 - 그냥 저냥한 문제, StringBuilder, charAt 사용하기 -

--- programmers/src/coding_basic_training/day5/ConcatenatedNumber.java
+++ programmers/src/coding_basic_training/day5/ConcatenatedNumber.java
@@ -1,4 +1,58 @@
 package coding_basic_training.day5;
 
+import java.util.ArrayList;
+
 public class ConcatenatedNumber {
+
+    public static void main(String[] args) {
+        // 테스트용 배열을 만듭니다.
+        int[] testArray = {3, 4, 5, 2, 1};
+
+        // 만든 배열을 인자로 넣어 solution 메소드를 호출합니다.
+        int result = solution(testArray);
+
+        // solution 메소드가 반환한 최종 결과를 출력해봅니다.
+        System.out.println("최종 결과: " + result);
+    }
+
+    public static int solution(int[] num_list) {
+        int answer = 0;
+
+        /*
+        * 1. 홀수리스트
+        * 2. 짝수리스트
+        *
+        * 각 리스트 만들어서 조건문써서 넣으면 될듯
+        * 이어붙일때 concat 쓰면 될듯?
+        *
+        * */
+
+        //빈배열 만들고 싶은데
+        //이럴때 ArrayList 를 사용하는건가?
+        ArrayList<Integer> odd_list = new ArrayList<>();
+        ArrayList<Integer> even_list = new ArrayList<>();
+
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even_list.add(num_list[i]);
+            } else {
+                odd_list.add(num_list[i]);
+            }
+        }
+
+        StringBuilder even_str = new StringBuilder();
+        StringBuilder odd_str = new StringBuilder();
+
+        for (int str : even_list) {
+            even_str.append(str);
+        }
+        for (int str : odd_list) {
+            odd_str.append(str);
+        }
+
+
+        answer = Integer.parseInt(even_str.toString()) + Integer.parseInt((odd_str.toString()));
+
+        return answer;
+    }
 }
--- programmers/src/coding_basic_training/day5/ProcessCode.java
+++ programmers/src/coding_basic_training/day5/ProcessCode.java
@@ -9,37 +9,55 @@ public class ProcessCode {
     }
 
     public static String solution(String code) {
+        String answer = "";
 
-        StringBuilder answer = new StringBuilder();
+        //변수 : ret(String), mode(String), idx(int, 0~ code.length - 1)
+        //code[idx] == mode ("0" or "1")
+        /*
+        * 문제 조건
+        * - 시작할때 모드는 0
+        * - return 하려는 ret이 빈문자열이라면 "Empty"를 return
+        *
+        *
+        *
+        *
+        * */
 
-        int mode = 0;
+        StringBuilder ret = new StringBuilder();
+        String mode = "0";
 
-        char[] chars = code.toCharArray();
+        char[] ch_code = code.toCharArray();
 
         for (int idx = 0; idx < code.length(); idx++) {
-            if (mode == 0) {
-                if (chars[idx] == '1') {
-                    mode = 1;
-                } else {
-                    if (idx % 2 == 0) {
-                        answer.append(chars[idx]);
-                    }
+
+//            System.out.println("ch_code = " + ch_code[idx]);
+
+            if (mode.equals("0") && ch_code[idx] == '1') {
+//                System.out.println("hello");
+                mode = "1";
+            } else if (mode.equals("0") && ch_code[idx] != '1') {
+                if (idx % 2 == 0) {
+                    ret.append(ch_code[idx]);
                 }
-            } else {
-                if (chars[idx] == '1') {
-                    mode = 0;
-                } else {
-                    if (idx % 2 == 1) {
-                        answer.append(chars[idx]);
-                    }
+            } else if (mode.equals("1") && ch_code[idx] == '1') {
+                mode = "0";
+            } else if (mode.equals("1") && ch_code[idx] != '1') {
+                if (idx % 2 == 1) {
+                    ret.append(ch_code[idx]);
                 }
             }
+
+//            System.out.println("mode = " + mode);
+
         }
 
-        if (answer.toString().equals("")) {
-            answer = new StringBuilder("Empty");
+        if (ret.isEmpty()) {
+            answer = "EMPTY";
+        } else {
+            answer = ret.toString();
         }
-        return answer.toString();
+
+        return answer;
     }
 
 }
