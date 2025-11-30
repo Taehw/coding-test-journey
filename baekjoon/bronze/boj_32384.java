24b5093eba84cf64f252a1578533b3da3e47b747
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-11-30 21:59:43 +0900

    백준32384 - 사랑은고려대입니다

---
+++ baekjoon/bronze/boj_32384.java
@@ -0,0 +1,16 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class boj_32384 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.print("LoveisKoreaUniversity ");
+        }
+    }
+}
