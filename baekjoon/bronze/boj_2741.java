8e13e4479a9405614bea387e5584e8a79f85c85a
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2025-12-13 23:50:12 +0900

    solved.ac class 1 dump - 2741 N찍기 - 2753 윤년 - 10171 고양이 - 10172 개

---
+++ baekjoon/bronze/boj_2741.java
@@ -0,0 +1,17 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class boj_2741 {
+
+    public static void main(String[] args) {
+
+        //브5 - N찍기
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ baekjoon/bronze/boj_2753.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.io.*;
+
+public class boj_2753 {
+
+    //브5 - 윤년
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+
+        System.out.println((year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 1 : 0);
+    }
+}
